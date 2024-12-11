38f42c4d338db38527f1aa485d3b43a4ba158d8e
David Achilles <david.achilles@accenture.com>
2024-12-11 15:19:23 +0100

    fix import

--- src/tonie_sync/clients/__init__.py
+++ src/tonie_sync/clients/__init__.py
@@ -1,5 +1,5 @@
 # from .spotify_client import SpotifyClient
-from .spotdl_client import SpotDLClient
+from .spotdl_client import SpotDLClient, SpotDLClientFactory
 from .tonie_client import TonieClient
 
-__all__ = ["TonieClient", "SpotDLClient"]
+__all__ = ["TonieClient", "SpotDLClient", "SpotDLClientFactory"]
