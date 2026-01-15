3c17d09ce50f0432aeebcc94243d863b21eb149d
David Achilles <david-achilles@hotmail.de>
2026-01-15 14:56:15 +0000

    update

--- src/tonie_sync/clients/youtube_client.py
+++ src/tonie_sync/clients/youtube_client.py
@@ -64,10 +64,10 @@ class YoutubeClient:
                     )
                     tracks_metadata.append(track_metadata)
             else:
-                track = YouTube(query)
+                track = YouTube(query, "WEB")
                 track_metadata = YoutubeTrackMetadata(
                     artists=[track.author],
-                    download_root=self.target_directory,
+                    # download_root=self.target_directory,
                     name=track.title,
                     duration_ms=track.length * 1000,  # convert to ms
                     album_name=None,
